a3cc215a638a3be05065cf6f21022cec3ef604a3
tanitta <ttata.trit@gmail.com>
2016-06-11 17:21:54 +0900

    Unify indent.

--- examples/rx-dlangui/source/app.d
+++ examples/rx-dlangui/source/app.d
@@ -14,25 +14,25 @@ mixin APP_ENTRY_POINT;
 /// entry point for dlangui based application
 extern (C) int UIAppMain(string[] args) {
 
-	auto window = createAppWindow();
+    auto window = createAppWindow();
 
     auto counter = window.mainWidget.childById!EditLine("counter");
 
-	auto appModel = new SampleModel;
+    auto appModel = new SampleModel;
 
-	appModel.counter.doSubscribe((int n){
-		counter.text = to!dstring(n);
-	});
-	counter.text = to!dstring(appModel.count);
+    appModel.counter.doSubscribe((int n){
+        counter.text = to!dstring(n);
+    });
+    counter.text = to!dstring(appModel.count);
 
-	Disposable[] events;
-	events ~= window.mainWidget.childById!Button("btnIncrement").click.asObservable().doSubscribe((Widget w) { appModel.increment(); });
-	events ~= window.mainWidget.childById!Button("btnDecrement").click.asObservable().doSubscribe((Widget w) { appModel.decrement(); });
+    Disposable[] events;
+    events ~= window.mainWidget.childById!Button("btnIncrement").click.asObservable().doSubscribe((Widget w) { appModel.increment(); });
+    events ~= window.mainWidget.childById!Button("btnDecrement").click.asObservable().doSubscribe((Widget w) { appModel.decrement(); });
 
-	window.mainWidget.childById!Button("btnDetach").click = (Widget w) {
-		foreach (e; events) e.dispose();
-		return true;
-	};
+    window.mainWidget.childById!Button("btnDetach").click = (Widget w) {
+        foreach (e; events) e.dispose();
+        return true;
+    };
 
     // close window on Close button click
     window.mainWidget.childById!Button("btnClose").click = delegate(Widget w) {
@@ -72,19 +72,19 @@ Window createAppWindow()
             // arrange some checkboxes horizontally
             HorizontalLayout {
                 layoutWidth: fill
-	            TextWidget { text: "Counter" }
-	            EditLine { id: counter; text: "some text"; layoutWidth: fill }
+                TextWidget { text: "Counter" }
+                EditLine { id: counter; text: "some text"; layoutWidth: fill }
 
-				Button { id: btnIncrement; text: "+1" }
-				Button { id: btnDecrement; text: "-1" }
+                Button { id: btnIncrement; text: "+1" }
+                Button { id: btnDecrement; text: "-1" }
             }
 
-			Button { id: btnDetach; text: "Detach" }
+            Button { id: btnDetach; text: "Detach" }
 
-			Button { id: btnClose; text: "Close" }
+            Button { id: btnClose; text: "Close" }
         }
     });
 
 
-	return window;
+    return window;
 }
--- examples/rx-dlangui/source/event.d
+++ examples/rx-dlangui/source/event.d
@@ -6,61 +6,61 @@ import dlangui.core.signals;
 auto asObservable(T)(ref T signal)
 if (is(T == Signal!U, U) && is(U == interface))
 {
-	static if (is(T == Signal!U, U))
-	{
-		import std.traits;
-		alias return_t = ReturnType!(__traits(getMember, U, __traits(allMembers, U)[0]));
-		alias param_t = ParameterTypeTuple!(__traits(getMember, U, __traits(allMembers, U)[0]));
-		static assert(param_t.length == 1);
-	}
+    static if (is(T == Signal!U, U))
+    {
+        import std.traits;
+        alias return_t = ReturnType!(__traits(getMember, U, __traits(allMembers, U)[0]));
+        alias param_t = ParameterTypeTuple!(__traits(getMember, U, __traits(allMembers, U)[0]));
+        static assert(param_t.length == 1);
+    }
 
-	static struct LocalObservable
-	{
-		alias ElementType = param_t[0];
-		this(ref T signal)
-		{
-			_subscribe = (Observer!ElementType o) {
-				auto dg = (ElementType w) {
-					o.put(w);
-					static if (is(return_t == bool))
-					{
-						return true;
-					}
-				};
+    static struct LocalObservable
+    {
+        alias ElementType = param_t[0];
+        this(ref T signal)
+        {
+            _subscribe = (Observer!ElementType o) {
+                auto dg = (ElementType w) {
+                    o.put(w);
+                    static if (is(return_t == bool))
+                    {
+                        return true;
+                    }
+                };
 
-				signal.connect(dg);
+                signal.connect(dg);
 
-				return new AnonymouseDisposable({
-					signal.disconnect(dg);
-				});
-			};
-		}
+                return new AnonymouseDisposable({
+                        signal.disconnect(dg);
+                        });
+            };
+        }
 
-		auto subscribe(U)(U observer)
-		{
-			return _subscribe(observerObject!ElementType(observer));
-		}
+        auto subscribe(U)(U observer)
+        {
+            return _subscribe(observerObject!ElementType(observer));
+        }
 
-		Disposable delegate(Observer!ElementType) _subscribe;
-	}
+        Disposable delegate(Observer!ElementType) _subscribe;
+    }
 
-	return LocalObservable(signal);
+    return LocalObservable(signal);
 }
 
 class AnonymouseDisposable : Disposable
 {
 public:
-	this(void delegate() dg)
-	{
-		_dispose = dg;
-	}
+    this(void delegate() dg)
+    {
+        _dispose = dg;
+    }
 public:
-	void dispose()
-	{
+    void dispose()
+    {
         if (_dispose) _dispose();
         _dispose = null;
-	}
+    }
 
 private:
-	void delegate() _dispose;
+    void delegate() _dispose;
 }
